8849c27998246766c79ef00f69ec8686f28598f3
luifer1018 <80869433+luifer1018@users.noreply.github.com>
2023-10-01 20:57:52 -0500

    Update WiroLimo.py

--- WiroLimo.py
+++ WiroLimo.py
@@ -31,14 +31,16 @@ if pagina_seleccionada == "Homepage":
         "<h1 style='text-align:center; color: black;'>Welcome to WiroLimo Transport</h1>",
         unsafe_allow_html=True,
         )   
-   
-    # Cargar las imágenes
-   
-    image1 = Image("imagenes Wirolimo/SUV.png")
-    st.image(image1, caption='SUV Image', use_column_width=True)
-    image2 = Image("https://i.imgur.com/80oMhJ1.jpg")
-    st.image(image2, caption='Sedan Image', use_column_width=True)
-        
+   with st.container():
+       colum1, colum2 = st.columns(2)
+        # Cargar las imágenes
+       with colum1:
+        image1 = Image.open("imagenes Wirolimo/SUV.png")
+        st.image(image1, caption='SUV Image', use_column_width=True)
+       with colum2    
+        image2 = Image.open("https://i.imgur.com/80oMhJ1.jpg")
+        st.image(image2, caption='Sedan Image', use_column_width=True)
+            
 
     # Dividir el diseño en dos columnas
     col1, col2 = st.columns(2)
