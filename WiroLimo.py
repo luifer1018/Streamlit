df6652c77e4d93f979124caf8c68f9879edfe947
luifer1018 <80869433+luifer1018@users.noreply.github.com>
2023-10-01 19:21:56 -0500

    Update WiroLimo.py

--- WiroLimo.py
+++ WiroLimo.py
@@ -36,7 +36,7 @@ if pagina_seleccionada == "Homepage":
      
     image1 = Image.open("https://i.imgur.com/6JK8z3Y.png")
     st.image(image1, caption='SUV Image', use_column_width=True)
-    image2 = Image.open("https://i.imgur.com/ssjpnAG.png")
+    image2 = Image.open("https://imgur.com/ssjpnAG")
     st.image(image2, caption='Sedan Image', use_column_width=True)
         
 
